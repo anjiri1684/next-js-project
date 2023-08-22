d99b507a0ee441756dece6ca46118f3da447480d
vincent anjiri <vincentanjiri12@gmail.com>
2023-08-22 21:04:02 +0300

    first commit

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function UserProfile({params}: any) {
+  return (
+    <div className=" flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p className="text-4xl">Profile Page 
+      <span className="p-2 rounded bg-gray-500 text-white ml-2"> {params.id}</span>
+      </p>
+    </div>
+  );
+}
